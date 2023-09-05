4392cffd6184e4cac47c1684b9121d4e86db6511
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-09-05 11:06:14 +0200

    nu spiller det helt

--- script.js
+++ script.js
@@ -4,41 +4,33 @@ const curseWords = [
   { bad: "marquee", good: "just don't" },
 ];
 
-let text = "Programmering er sjovt! Men husk altid at bruge var, float og marquee i moderne kodning.";
-document.querySelector("p").textContent = text;
-console.log(text);
-let textGood;
+let textGood = "Programmering er sjovt! Men husk altid at bruge var, float og marquee i moderne kodning.";
+document.querySelector("p").textContent = textGood;
+console.log(textGood);
+// let textGood;
 
 const filter_btn = document.querySelector("#bullshit");
 
-filter_btn.addEventListener("click", filter);
+filter_btn.addEventListener("click", klik);
+
+function klik() {
+  if (isItsafe()) {
+    console.log("***** It is safe ******");
+    document.querySelector("dialog").showModal();
+  } else {
+    filter();
+  }
+}
+function isItsafe() {
+  return !curseWords.some((item) => textGood.includes(item.bad));
+}
 
 function filter() {
-  filter_btn.removeEventListener("click", filter);
-  textGood = text;
-  console.log("original is:", text);
+  console.log("original is:", textGood);
 
   curseWords.forEach((word) => {
-    textGood = textGood.replace(word.bad, word.good);
-    // word.good.style.background = "lightgreen";
+    textGood = textGood.replace(word.bad, `<span>${word.good}</span>`);
+    document.querySelector("p").innerHTML = textGood;
+    console.log("new is:", textGood);
   });
-
-  console.log("new is:", textGood);
-  document.querySelector("p").textContent = textGood;
-
-  //   *** forsøg på at ændre de 3 ord
-  if (textGood.includes("const")) {
-    // textGood.replace("cosnt", "<b>" + "const");
-    // textGood["const"].style.background = "red";
-    // const highlight = textGood.split("const");
-    // console.log(highlight);
-    // textGood
-    // document.querySelector("p").style.background = "red";
-  }
-
-  //   *** forsøg på alert ***
-  //   if (textGood !== text && textGood !== "") {
-  //     alert("Bullshit filter applied!");
-  //   }
-  filter_btn.addEventListener("click", filter);
 }
